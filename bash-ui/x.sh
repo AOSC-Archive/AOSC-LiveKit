3c42ccfa98ab03679c73ae6e371e0f4b61330ed7
Icenowy Zheng <icenowylin@gmail.com>
2014-01-10 19:44:17 +0800

    Haha

--- bash-ui/x.sh
+++ bash-ui/x.sh
@@ -1,9 +1,9 @@
 if [ "$DESKTOP_SESSION" = "mate" ]
 then
 	. $BUI_PREFIX/mate-dialog.sh
-elif [ "$DESKTOP_SESSION" = "kde" ]
-then
-	. $BUI_PREFIX/kdialog.sh
+#elif [ "$XDG_CURRENT_DESKTOP" = "KDE" ]
+#then
+#	. $BUI_PREFIX/kdialog.sh
 else
 	. $BUI_PREFIX/zenity.sh
 fi
---
+++ bash-ui/zenity.sh
@@ -0,0 +1,22 @@
+GDPROV=zenity
+bui-msgbox(){
+	$GDPROV --info --text="$1"
+}
+bui-choice(){
+	$GDPROV --question --text="$1"
+}
+bui-working(){
+	$GDPROV --progress --pulsate --text="$1" --no-cancel --auto-close
+}
+bui-inputbox(){
+	$GDPROV --entry --text="$1"
+}
+bui-progress(){
+	$GDPROV --progress --text="$1" --no-cancel --auto-close
+}
+bui-list(){
+	BUILISTTEXT="$1"
+	BUILISTCOLUMN="$2"
+	shift 2
+	( for i in $* ; do echo $i ; done ) | $GDPROV --list --text="$BUILISTTEXT" --column="$BUILISTCOLUMN" --editable
+}
