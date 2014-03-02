8d24dfe45489af9de856a96b2ea5c67949a86c61
Jeff Bai <jeffbaichina@members.fsf.org>
2014-03-01 23:01:38 -0700

    L10n emergence

---
+++ Qt-ui/l10n/AOSC-Installer_en-us.ts
@@ -0,0 +1,386 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>DoneTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="734"/>
+        <source>æ­£å¨è¿è¡æç»éç½®.....</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="744"/>
+        <source>ç»æ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="750"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="750"/>
+        <source>éç½®ç¯èåºç°è´å½éè¯¯</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="754"/>
+        <source>å·¥ä½åæ»¡å®æ ;-)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="755"/>
+        <source>è¯·æå¨éå¯è®¡ç®æºæ¥å¯å¨æ°ç³»ç»</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GPartedDiskTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="335"/>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="352"/>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="412"/>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="439"/>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="456"/>
+        <source>---</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="367"/>
+        <source>ç£çååºä¸­â¦â¦</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="370"/>
+        <source>&lt;h2&gt;&lt;font color=red&gt;è¿äºå¿å¾éè¦ï¼è¯·æ£æ¥æ¸æ¥åç»§ç»­!&lt;/font&gt;&lt;/h2&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="374"/>
+        <source>ç¹å»ææå¼ç£çååºè½¯ä»¶</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="377"/>
+        <source>è¯·éæ©ä½ çä¸»ååº</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="380"/>
+        <source>æ ¼å¼å</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="385"/>
+        <source>æ¯å¦ä¸ºEFIè®¾å¤</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="391"/>
+        <source>è¯·éæ©ä½ çå¼å¯¼è®¾å¤</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="495"/>
+        <source>è¯·éæ©ä¸ä¸ªååº</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="499"/>
+        <source>è¯·éæ©å¼å¯¼è®¾å¤</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="504"/>
+        <source>è¯·éæ©ä¸EFIä¸ªååº</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="509"/>
+        <source>ç¡®å®å¼å§å®è£?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="513"/>
+        <source>ä½ ç¡®å®è¦æ ¼å¼åæ­¤ååº?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="520"/>
+        <source>OMG, æ ¼å¼åååºå¤±è´¥!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GetStartedTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="239"/>
+        <source>åå¤å¼å§</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="240"/>
+        <source>æä»¬æ¥ä¸æ¥éè¦åè¿äºäºæâ¦â¦</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="241"/>
+        <source> - è¿è¡å¤§éçéè¯»ï¼æè® 
+
+ - å°ç£çååº
+
+ - è®©æä»¬è®¤è¯ä¸ä¸ä½ 
+
+ - å¼å§å®è£
+
+ - å°½æäº«ç¨!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWorkTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="546"/>
+        <source>ç³»ç»å·²å®æå®è£åçåå¤</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="555"/>
+        <source>Click to Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="564"/>
+        <source>æ­£å¨åå¤å®è£æä»¶</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="570"/>
+        <source>æè½½ç£çåºé!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="577"/>
+        <source>æ­£å¨å¤å¶å®è£æä»¶</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="578"/>
+        <source>ç³»ç»æ­£å¨å®è£â¦â¦</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="590"/>
+        <source>åºç¡ç³»ç»å®è£å®æ¯</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="591"/>
+        <source>æ­£å¨å®è£Grub......</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="593"/>
+        <source>ç³»ç»å®è£åºé!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="600"/>
+        <source>å®è£ GRUB å¤±è´¥!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="603"/>
+        <source>GRUB å®è£æå!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="604"/>
+        <source>æ­£å¨éç½®Grub......</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="610"/>
+        <source>æ´æ° /etc/fstab å¤±è´¥!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="613"/>
+        <source>æ´æ° /etc/fstab æå!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="614"/>
+        <source>åºç¡ç³»ç»å®è£å®æ¯!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="622"/>
+        <source>GRUB éç½®å¤±è´¥!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="625"/>
+        <source>GRUB éç½®æå!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProgressTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="47"/>
+        <source>Welcome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="52"/>
+        <source>GetStart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="57"/>
+        <source>Reading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="62"/>
+        <source>GPartedDisk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="107"/>
+        <source>MainWork</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="131"/>
+        <source>User</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="132"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProgressTabWidget</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="158"/>
+        <source>åè¿</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="159"/>
+        <source>åé</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReadingTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="264"/>
+        <source>éè¯»æ¶é´!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="268"/>
+        <source>æä¿è¯ä¼ä¹ä¹å°éµå®æ­¤è®¸å¯åè®®!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="284"/>
+        <source>æå¼è®¸å¯æä»¶</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="284"/>
+        <source>æå¼è®¸å¯æä»¶æ¶åºé!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="643"/>
+        <source>åè¯æä»¬ä½ æ¯è°</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="647"/>
+        <source>è®¾ç½®Rootçå¯ç </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="653"/>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="675"/>
+        <source>åæ¬¡è¾å¥å¯ç </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="662"/>
+        <source>è®¾ç½®ä½ ä¸»ç¨æ·çç¨æ·å</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="673"/>
+        <source>è®¾ç½®ä½ ä¸»ç¨æ·çå¯ç </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="691"/>
+        <source>è¯·è®¾ç½®Rootå¯ç </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="695"/>
+        <source>Rootå¯ç ä¸ä¸è´</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="699"/>
+        <source>è¯·è®¾ç½®ä¸»ç¨ççç¨æ·å</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="703"/>
+        <source>è¯·è®¾ç½®ä¸»ç¨æ·çå¯ç </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="707"/>
+        <source>ç¨æ·å¯ç ä¸ä¸è´</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="710"/>
+        <source>æ ¸å¯¹ä¿¡æ¯æ è¯¯è¯·ç»§ç»­</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="716"/>
+        <source>Root ç¨æ·éç½®å¤±è´¥!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="723"/>
+        <source>è®¾ç½®ç¨æ·æ°æ®å¤±è´¥!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WelcomeTab</name>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="216"/>
+        <source>è¯¶ï¼ä½ å¥½!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/AOSC-Installer-GUI.cpp" line="220"/>
+        <source>æè°¢å°è¯å®åå¼æºç¤¾åºææ°çåè¡ç!
+
+ä½ åå¤å¥½å°åè¡çå®è£å°ä½ äº²ç±ççµèä¸äºä¹?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
